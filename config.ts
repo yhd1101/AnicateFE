5c92e2c63f8c43166d2c7f056cfcf45e16947c21
ohamin26 <ohamin26@gmail.com>
2024-12-15 14:50:48 +0900

    chore : 프로젝트 초기 세팅팅

---
+++ config.ts
@@ -0,0 +1,6 @@
+declare global {
+  interface ImportMeta {
+    readonly env: ImportMeta;
+  }
+}
+export const CONFIG = {} as const;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  base: process.env.NODE_ENV === 'development' ? '/' : './',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name?.split('.').at(1) || 'unknown';
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
+});
